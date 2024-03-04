497d95f979670271296c266bfd8fab2a77cb0f68
akhdrzki <akhadirizky@gmail.com>
2024-03-05 05:38:18 +0800

    add: init navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useMe } from "../../hooks/useMe.jsx";
+
+export const Navbar = ({username}) => {
+
+  return (
+    <div className="navbar bg-base-100 bg-neutral text-neutral-content">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">@{username}</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Link</a>
+          </li>
+          <li>
+            <a>Link</a>
+          </li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="p-2 bg-base-100 rounded-t-none">
+                <li>
+                  <a>Link 1</a>
+                </li>
+                <li>
+                  <a>Link 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
