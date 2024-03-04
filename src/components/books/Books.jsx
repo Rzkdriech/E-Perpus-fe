ea7ad6c07c5da081cdb216c6dc6f9215baa49c86
akhdrzki <akhadirizky@gmail.com>
2024-03-05 05:38:00 +0800

    add: fetching data books

--- src/components/books/Books.jsx
+++ src/components/books/Books.jsx
@@ -8,7 +8,7 @@ export const Books = () => {
 
   const fetchBooks = async () => {
     try {
-      const res = await axiosInstance.get("/books");
+      const res = await axiosInstance.get("/relCategories");
       setBooks(res.data)
     } catch (error) {
       console.log(error);
@@ -20,16 +20,17 @@ export const Books = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Bookz</h1>
-      <div className="books">
+    <div className="container mx-auto">
+      <h1 className="text-2xl font-bold mb-4">List Books</h1>
+      <div className="books grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {books.map((book) => (
-          <div className="book" key={book.BukuID}>
-            {book.Cover && <img src={book.Cover} alt="gambar" />}
-            <h2>{book.Judul}</h2>
-            <p>{book.Penulis}</p>
-            <p>{book.Penerbit}</p>
-            <p>{book.TahunTerbit}</p>
+          <div className="book border border-gray-200 rounded-lg p-4 mb-8" key={book.BukuID}>
+            {book.buku.Cover && <img src={book.buku.Cover} alt="gambar" className="mb-2 rounded-lg" />}
+            <h2 className="text-lg font-semiboldi text-center">{book.buku.Judul}</h2>
+            <p className="text-sm text-gray-600 text-center">{book.kategoribuku.NamaKategori}</p>
+            <p className="text-sm text-center">{book.buku.Penulis}</p>
+            <p className="text-sm text-center">{book.buku.Penerbit}</p>
+            <p className="text-sm text-center">{book.buku.TahunTerbit}</p>
           </div>
         ))}
 
