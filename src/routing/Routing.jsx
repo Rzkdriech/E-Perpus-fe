af3a69f2544be3f07c14c1bc571c193f6387fd6c
akhdrzki <akhadirizky@gmail.com>
2024-03-04 17:52:26 +0800

    init a login route

--- src/routing/Routing.jsx
+++ src/routing/Routing.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home/Home.jsx";
-
+import { Login } from "../pages/Auth/Login.jsx"
 
 export const Routing = () => {
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route exact path="/">
+
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="/home" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
   );
